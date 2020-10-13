178a8cb4136f2a9caba7b01ef54efde6f4fb3e68
Stephen Huang <huangstephen9@gmail.com>
2020-10-13 12:36:52 -0400

    Users routes

--- server/api/index.js
+++ server/api/index.js
@@ -2,9 +2,6 @@ const router = require('express').Router()
 module.exports = router
 
  router.use('/users', require('./users'))
-// router.use('/cheeses', require('./cheeses'))
-// router.use('/cheeseCart', require('./cheeseCart'))
-// router.use('/cart', require('./cart'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
