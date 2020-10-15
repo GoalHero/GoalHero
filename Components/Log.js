4f3b519754591d2720189413d80a86c57af2868f
ericfish12 <ericfish1221@gmail.com>
2020-10-14 23:58:38 -0400

    login and signup works but not redux

---
+++ Components/Log.js
@@ -0,0 +1,145 @@
+import React, { useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Button,
+  TextInput,
+  TouchableHighlight,
+  Alert,
+} from "react-native";
+import { StatusBar } from "expo-status-bar";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import User from "./User";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import {connect} from 'react-redux'
+import {auth} from '../Store/user'
+
+const Log = (props) => {
+   // props.navigation.navigate("SignPage")
+  //  console.log(props.route)
+  const { handleSubmit, register, setValue } = useForm();
+  const onSubmit=async(values)=>{
+
+    try {
+        
+    
+         
+          const formName = 'login'
+          const email = values.email
+          const password = values.password
+        await  props.login(email,password,formName)
+         
+         const res = await axios.get('http://localhost:8080/auth/me')
+       //  console.log("res.data",res.data)
+         //console.log("&&&&&",props.user,"^^^^^^^^^^^")
+         if(!res.data) throw new Error;
+          Alert.alert("Congratulations")
+          props.navigation.navigate("HomeScreen")
+      } catch (error) {
+          Alert.alert("Invalid Input")
+      }
+  }
+  
+
+  useEffect(() => {
+    register("email");
+    register("password");
+  }, [register]);
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>Start Your GoalHero Journey!</Text>
+      <TextInput
+        placeholder="Email:"
+        style={styles.textInputStyle}
+        onChangeText={(text) => setValue("email", text)}
+      />
+      <TextInput
+        placeholder="Password:"
+        password={true}
+        style={styles.textInputStyle}
+        onChangeText={(text) => setValue("password", text)}
+      />
+
+      <View style={styles.buttonStyle}>
+        <Button title="Login" onPress={handleSubmit(onSubmit)} />
+      </View>
+      <View style={styles.buttonStyle}>
+        <Button
+          title="Signup"
+          onPress={() => props.navigation.navigate("SignPage")}
+        />
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    height: 666,
+    // flex: 0,
+    //   flexDirection:"row",
+    //   flexWrap: "wrap",
+    backgroundColor: "#dddddd",
+    alignItems: "center",
+    // justifyContent: 'center',
+  },
+  img: {
+    width: 55,
+    height: 55,
+    borderRadius: 200 / 2,
+  },
+
+  welcome: {
+    marginTop: 111,
+    fontSize: 28,
+    marginBottom: 99,
+  },
+
+  textInputStyle: {
+    marginTop: 8,
+    width: "100%",
+    height: 38,
+    backgroundColor: "white",
+    borderRadius: 100 / 50,
+  },
+  buttonStyle: {
+    backgroundColor: "pink",
+    width: 200,
+    height: 40,
+    borderRadius: 200 / 20,
+    marginTop: 55,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+
+
+const mapLogin = state => {
+    return {
+     
+      user: state.user
+    }
+  }
+  
+  const mapDispatch = dispatch => {
+   
+    return {
+    
+     login:(email, password, formName)=>
+        dispatch(auth(email, password, formName))
+      
+    }
+  }
+  
+
+
+
+
+  export default connect(mapLogin, mapDispatch)(Log)
--- Components/Login.js
+++ Components/Login.js
@@ -7,25 +7,26 @@ import { createStackNavigator } from "@react-navigation/stack";
 import User from './User'
 import { useForm } from "react-hook-form";
 import axios from 'axios'
-
-
-const  gg = async()=>{
-
-//await axios.delete('http://localhost:3333/2')
-
-const data = await axios.get('https://grace-cheese-prime.herokuapp.com/api/cheeses')
-console.log(data.data.length)
-}
-
+import Log from './Log'
+import Sign from './Sign'
+import HomeScreen from './HomeScreen'
 
 
 
 
 
 const Stack = createStackNavigator();
-console.log('2222')
-gg()
-const Log = ({ navigation }) => {
+// const  gg = async()=>{
+
+//   //await axios.delete('http://localhost:3333/2')
+//   navigation.navigate("SignPage")
+//   const data = await axios.get('https://grace-cheese-prime.herokuapp.com/api/cheeses')
+//   console.log(data.data.length)
+//   }
+  
+// console.log('2222')
+// gg()
+// const Log = ({ navigation }) => {
 
 
 
@@ -34,83 +35,83 @@ const Log = ({ navigation }) => {
 
 
   
-  const { handleSubmit, register, setValue } = useForm();
-  const onSubmit = values => alert(values.email);
+//   const { handleSubmit, register, setValue } = useForm();
+//   const onSubmit = values => alert(values.email);
 
-  useEffect(()=>{
-    register("email");
-    register("password");
-  },[register])
-  return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Start Your GoalHero Journey!</Text>
-      <TextInput placeholder="Email:" style={styles.textInputStyle} onChangeText={text=>setValue("email",text)}/>
-      <TextInput
-        placeholder="Password:"
-        password={true}
-        style={styles.textInputStyle}
-        onChangeText={text=>setValue("password",text)}
-      />
+//   useEffect(()=>{
+//     register("email");
+//     register("password");
+//   },[register])
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.welcome}>Start Your GoalHero Journey!</Text>
+//       <TextInput placeholder="Email:" style={styles.textInputStyle} onChangeText={text=>setValue("email",text)}/>
+//       <TextInput
+//         placeholder="Password:"
+//         password={true}
+//         style={styles.textInputStyle}
+//         onChangeText={text=>setValue("password",text)}
+//       />
 
-      <View style={styles.buttonStyle}>
-        <Button
-          title="Login"
-          onPress={handleSubmit(onSubmit)}
-        />
-      </View>
-      <View style={styles.buttonStyle}>
-        <Button
-          title="Signup"
-          onPress={() => navigation.navigate("SignPage")}
-        />
-      </View>
-      <StatusBar style="auto" />
-    </View>
-  );
-};
-const Sign = ({ navigation }) => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Sign Up!</Text>
-      <TextInput placeholder="Name:" style={styles.textInputStyle} />
-      <TextInput placeholder="Email:" style={styles.textInputStyle} />
-      <TextInput
-        placeholder="Password:"
-        password={true}
-        style={styles.textInputStyle}
-      />
-      <View>{/* {heros.map(hero,index)=>{
-return(
-    <Image />
-)
-}} */}</View>
+//       <View style={styles.buttonStyle}>
+//         <Button
+//           title="Login"
+//           onPress={handleSubmit(onSubmit)}
+//         />
+//       </View>
+//       <View style={styles.buttonStyle}>
+//         <Button
+//           title="Signup"
+//           onPress={() => navigation.navigate("SignPage")}
+//         />
+//       </View>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// };
+// const Sign = ({ navigation }) => {
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.welcome}>Sign Up!</Text>
+//       <TextInput placeholder="Name:" style={styles.textInputStyle} />
+//       <TextInput placeholder="Email:" style={styles.textInputStyle} />
+//       <TextInput
+//         placeholder="Password:"
+//         password={true}
+//         style={styles.textInputStyle}
+//       />
+//       <View>{/* {heros.map(hero,index)=>{
+// return(
+//     <Image />
+// )
+// }} */}</View>
 
-      <View style={styles.buttonStyle}>
-      <Button
-          title="Signup"
-          onPress={() => navigation.navigate("LogPage")}
-        />
+//       <View style={styles.buttonStyle}>
+//       <Button
+//           title="Signup"
+//           onPress={() => navigation.navigate("LogPage")}
+//         />
 
-      </View>
+//       </View>
 
-      <StatusBar style="auto" />
-    </View>
-  );
-};
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// };
 
 export default class Login extends React.Component {
   render() {
     return (
       <NavigationContainer independent={true}>
         {/* Rest of your app code */}
-
+        
         <Stack.Navigator>
           <Stack.Screen
             name="LogPage"
             component={Log}
             options={{ title: "Welcome" }}
           />
-
+          <Stack.Screen name="HomeScreen" component={HomeScreen} />
           <Stack.Screen name="SignPage" component={Sign} />
           <Stack.Screen name="UserPage" component={User} />
         </Stack.Navigator>
@@ -119,42 +120,42 @@ export default class Login extends React.Component {
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    height: 666,
-    // flex: 0,
-    //   flexDirection:"row",
-    //   flexWrap: "wrap",
-    backgroundColor: "#dddddd",
-    alignItems: "center",
-    // justifyContent: 'center',
-  },
-  img: {
-    width: 55,
-    height: 55,
-    borderRadius: 200 / 2,
-  },
+// const styles = StyleSheet.create({
+//   container: {
+//     height: 666,
+//     // flex: 0,
+//     //   flexDirection:"row",
+//     //   flexWrap: "wrap",
+//     backgroundColor: "#dddddd",
+//     alignItems: "center",
+//     // justifyContent: 'center',
+//   },
+//   img: {
+//     width: 55,
+//     height: 55,
+//     borderRadius: 200 / 2,
+//   },
 
-  welcome: {
-    marginTop: 111,
-    fontSize: 28,
-    marginBottom: 99,
-  },
+//   welcome: {
+//     marginTop: 111,
+//     fontSize: 28,
+//     marginBottom: 99,
+//   },
 
-  textInputStyle: {
-    marginTop: 8,
-    width: "100%",
-    height: 38,
-    backgroundColor: "white",
-    borderRadius: 100 / 50,
-  },
-  buttonStyle: {
-    backgroundColor: "pink",
-    width: 200,
-    height: 40,
-    borderRadius: 200 / 20,
-    marginTop: 55,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+//   textInputStyle: {
+//     marginTop: 8,
+//     width: "100%",
+//     height: 38,
+//     backgroundColor: "white",
+//     borderRadius: 100 / 50,
+//   },
+//   buttonStyle: {
+//     backgroundColor: "pink",
+//     width: 200,
+//     height: 40,
+//     borderRadius: 200 / 20,
+//     marginTop: 55,
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
