7e9bb59f80e8dc747a7eec7cb8aa8a751b4cd3eb
Danielle Arquisola <64483418+daniellearquisola@users.noreply.github.com>
2020-10-15 22:02:56 -0500

    fix: added image background to goals page

--- Components/Goals.js
+++ Components/Goals.js
@@ -1,65 +1,83 @@
 import React, { Component, useEffect } from "react";
-import { View, Text, Button, StyleSheet, StatusBar, TextInput } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  StatusBar,
+  TextInput,
+  ImageBackground,
+} from "react-native";
 import { useForm, useState } from "react-hook-form";
-import { connect } from "react-redux"
-import { fetchGoals, removeGoal, completeGoal, postGoal } from "../Store/goals"
-
-
+import { connect } from "react-redux";
+import { fetchGoals, removeGoal, completeGoal, postGoal } from "../Store/goals";
 
 const Goals = (props) => {
   const { handleSubmit, register, setValue } = useForm();
 
-  useEffect(()=>{
+  useEffect(() => {
     register("goalName");
 
     props.fetchGoals();
-  },[register])
+  }, [register]);
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.goalHeading}>Goals!</Text>
-      <View style={styles.goalList}>
-        {props.goals.map((goal) => {
-          if (!goal.completed) {
-            return (
-              <View key={goal.id} style={styles.goalRow}>
-                <Text style={styles.orangeBox}>{goal.name}</Text>
-                <Button title="Complete" onPress={() => props.completeGoal(goal.id)}></Button>
-                <Button title="Remove" onPress={() => props.removeGoal(goal.id)}></Button>
-              </View>
-            )
-          }
-        })}
-      </View>
+    <ImageBackground
+      style={styles.background}
+      source={require("../assets/images/game_background_1.png")}
+    >
+      <View style={styles.container}>
+        <Text style={styles.goalHeading}>YOUR GOALS</Text>
+        <View style={styles.goalList}>
+          {props.goals.map((goal) => {
+            if (!goal.completed) {
+              return (
+                <View key={goal.id} style={styles.goalRow}>
+                  <Text style={styles.orangeBox}>{goal.name}</Text>
+                  <Button
+                    title="Complete"
+                    onPress={() => props.completeGoal(goal.id)}
+                  ></Button>
+                  <Button
+                    title="Remove"
+                    onPress={() => props.removeGoal(goal.id)}
+                  ></Button>
+                </View>
+              );
+            }
+          })}
+        </View>
 
-      <View style={styles.addGoal}>
-        <Text style={styles.increaseVerticalMargin}>Add New Goal:</Text>
-        <TextInput placeholder="Input Box" style={styles.inputBox} onChangeText={text => setValue("goalName", text)}/>
-        <View style={styles.topMargin}>
-          <Button
-            title="Add Goal"
-            onPress={handleSubmit(props.postGoal)}
+        <View style={styles.addGoal}>
+          <Text style={styles.increaseVerticalMargin}>Add A New Goal:</Text>
+          <TextInput
+            placeholder="Input Box"
+            style={styles.inputBox}
+            onChangeText={(text) => setValue("goalName", text)}
           />
+          <View style={styles.topMargin}>
+            <Button title="Add" onPress={handleSubmit(props.postGoal)} />
+          </View>
         </View>
       </View>
-    </View>
+    </ImageBackground>
   );
 };
 
 const mapState = (state) => {
   return {
-    goals: state.goals
-  }
-}
+    goals: state.goals,
+  };
+};
 
 const mapDispatch = (dispatch) => {
   return {
     fetchGoals: (userId) => dispatch(fetchGoals(userId)),
     removeGoal: (id) => dispatch(removeGoal(id)),
     completeGoal: (id) => dispatch(completeGoal(id)),
-    postGoal: (values) => dispatch(postGoal(values))
-  }
-}
+    postGoal: (values) => dispatch(postGoal(values)),
+  };
+};
 
 export default connect(mapState, mapDispatch)(Goals);
 
@@ -71,7 +89,7 @@ const styles = StyleSheet.create({
   },
   goalList: {
     flexDirection: "column",
-    justifyContent: "space-evenly"
+    justifyContent: "space-evenly",
   },
   goalRow: {
     flexDirection: "row",
@@ -79,28 +97,32 @@ const styles = StyleSheet.create({
   },
   addGoal: {
     alignItems: "center",
-    marginTop: 100
+    marginTop: 100,
   },
   increaseVerticalMargin: {
-    marginBottom: 50
+    marginBottom: 50,
   },
   orangeBox: {
     backgroundColor: "orange",
     width: 120,
     height: 20,
-    textAlign: "center"
+    textAlign: "center",
   },
   inputBox: {
     backgroundColor: "orange",
     width: 160,
     height: 60,
-    textAlign: "center"
+    textAlign: "center",
   },
   goalHeading: {
     marginBottom: 50,
-    fontSize: 30
+    fontSize: 30,
   },
   topMargin: {
-    marginTop: 20
-  }
+    marginTop: 20,
+  },
+  background: {
+    flex: 1,
+    resizeMode: "cover",
+  },
 });
