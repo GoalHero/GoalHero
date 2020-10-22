1c38bf428e5f093f10c53d381f374799d43cd0d9
ericfish12 <ericfish1221@gmail.com>
2020-10-22 17:16:24 -0400

    rg

--- GameEngine/Play.js
+++ GameEngine/Play.js
@@ -13,6 +13,7 @@ import {
   Image,
   Alert,
   Button,
+  ImageBackground
 } from 'react-native';
 import Character from './entities/Character';
 import Floor from './entities/Floor';
@@ -96,22 +97,23 @@ export class Play extends React.Component {
     super();
     this.state = {
       rerender: true,
-      rendergame: true,
+      rendergame: false,
     };
   }
- async componentDidMount() {
-    // try {
-    //   this.battleSound = new Audio.Sound(); 
-    //   await this.battleSound.loadAsync(
-    //     require("../Sound/battleMusic/battle.mp3")
-    //   )
-    //   await this.battleSound.setIsLoopingAsync(true); 
-    //   await this.battleSound.playAsync()
-    // } catch (error) {
-    //   console.log("there was an issue play the battle sounds: ", error)
-    // }
+//  async componentDidMount() {
+  
+//     // try {
+//     //   this.battleSound = new Audio.Sound(); 
+//     //   await this.battleSound.loadAsync(
+//     //     require("../Sound/battleMusic/battle.mp3")
+//     //   )
+//     //   await this.battleSound.setIsLoopingAsync(true); 
+//     //   await this.battleSound.playAsync()
+//     // } catch (error) {
+//     //   console.log("there was an issue play the battle sounds: ", error)
+//     // }
 
-  }
+//   }
   render() {
     if (this.props.healthBar <= 0) {
       // this.props.setHP()
@@ -122,6 +124,7 @@ export class Play extends React.Component {
           {
             text: 'YES',
             onPress: async () => {
+              this.setState({rendergame:false})
               await this.props.healChar();
               await this.props.healMonster();
               this.props.navigation.navigate('Goals');
@@ -149,6 +152,7 @@ export class Play extends React.Component {
             text: "LET'S GO!",
             onPress: async () => {
               //  arr[0]=7
+              this.setState({rendergame:false})
               await this.props.updateKillTimesAndMonster();
               allMonsters.push(allMonsters.shift());
               await this.props.healChar();
@@ -247,9 +251,44 @@ export class Play extends React.Component {
       );
     } else {
       return (
-        <View>
-          <Button title="Play"></Button>
+        <ImageBackground
+        style={styles.background}
+        source={require('../assets/images/game_background_1.png')}
+      >
+        <View style={styles.container}>
+          <Image
+            style={{ width: 360, height: 140 }}
+            source={require('../assets/images/logotest.png')}
+          />
+          <Text style={styles.body}>
+            <Text>
+              The village needs a hero to fight the monsters! {'\n'}
+              Will you be the hero they need? {'\n\n\n'}
+              Go to the GOALS page and set personal goals. {'\n'}
+            </Text>
+            <Text style={{ color: 'black', fontStyle: 'italic' }}>
+              Finally start and finish that book. Work towards mastering a new
+              yoga pose. Wake up early for a run every day for a week. Finish
+              that last project you left behind. {'\n\n\n'}
+            </Text>
+            <Text>
+              Every time you complete a goal, your hero {'\n'} will become
+              stronger. Go to the PLAY page to fight the monster. Once you
+              defeat the {'\n'}monster, a new hero awaits your help! {'\n\n\n'}
+            </Text>
+            <Text>It's time to train yourself to be your own hero.</Text>
+          </Text>
+          <View>
+            <Text>{'\n\n\n'}</Text>
+          </View>
         </View>
+        <View style={styles.buttonStyle}>
+          <Button title="Play" onPress={()=>{this.setState({rendergame:true})}}></Button>
+        </View>
+      </ImageBackground>
+
+       
+      
       );
     }
   }
@@ -263,6 +302,49 @@ const styles = StyleSheet.create({
   },
   absolute: {
     position: 'absolute',
+  },
+  buttonStyle: {
+    backgroundColor: '#F09031',
+   // color: 'white',
+    width: 200,
+    height: 40,
+    borderRadius: 200 / 20,
+   // marginTop: 2,
+   // alignItems: 'center',
+   // justifyContent: 'center',
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    color: 'purple',
+  },
+  header: {
+    justifyContent: 'center',
+    fontSize: 20,
+    padding: 30,
+    color: 'black',
+  },
+  body: {
+    fontSize: 12,
+    textAlign: 'center',
+    backgroundColor: '#6A7B89',
+    padding: 10,
+    borderRadius: 20,
+    opacity: 0.7,
+    fontWeight: 'bold',
+    fontFamily: 'Menlo-Regular',
+    color: 'white',
+  },
+  logo: {
+    width: 280,
+    height: 280,
+    marginLeft: '20%',
+    marginTop: '10%',
+  },
+  background: {
+    flex: 1,
+    resizeMode: 'cover',
   },
 });
 
